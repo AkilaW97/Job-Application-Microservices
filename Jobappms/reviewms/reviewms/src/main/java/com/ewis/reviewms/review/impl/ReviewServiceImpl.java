ddf7d51958a9cccc7fc45d03522d21ffa6548137
Akila <akilaewis@gmail.com>
2025-08-18 21:02:46 +0530

    added review ms with the paste code from the monolothic application - not fixed

---
+++ Jobappms/reviewms/reviewms/src/main/java/com/ewis/reviewms/review/impl/ReviewServiceImpl.java
@@ -0,0 +1,76 @@
+package com.ewis.reviewms.review.impl;
+
+import com.ewis.jobapplication.company.Company;
+import com.ewis.jobapplication.company.CompanyService;
+import com.ewis.jobapplication.review.Review;
+import com.ewis.jobapplication.review.ReviewRepository;
+import com.ewis.jobapplication.review.ReviewService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+
+    private ReviewRepository reviewRepository;
+    private CompanyService companyService;
+
+
+    public ReviewServiceImpl(ReviewRepository reviewRepository, CompanyService companyService) {
+        this.reviewRepository = reviewRepository;
+        this.companyService = companyService;
+    }
+
+    @Override
+    public List<Review> getAllReviews(Long companyId) {
+        List<Review> reviews = reviewRepository.findByCompanyId(companyId);
+        return reviews;
+    }
+
+    @Override
+    public boolean addReview(Long companyId, Review review) {
+        Company company = companyService.getCompanyById(companyId);
+        if(company != null){
+            review.setCompany(company);
+            reviewRepository.save(review);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public Review getReview(Long companyId, Long reviewId) {
+        List <Review> reviews = reviewRepository.findByCompanyId(companyId);
+        return reviews.stream().filter(review -> review.getId() == reviewId)
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public boolean updateReview(Long companyId, Long reviewId, Review updatedReview) {
+        if(companyService.getCompanyById(companyId) != null){
+            updatedReview.setCompany(companyService.getCompanyById(companyId));
+            updatedReview.setId(reviewId);
+            reviewRepository.save(updatedReview);
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean deleteReview(Long companyId, Long reviewId) {
+        if(companyService.getCompanyById(companyId) != null && reviewRepository.existsById(reviewId)){
+            Review review = reviewRepository.findById(reviewId).orElse(null);
+            Company company = review.getCompany();
+            company.getReviews().remove(review);
+            review.setCompany(null);
+            companyService.updateCompany(company, companyId);
+            reviewRepository.deleteById(reviewId);
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
